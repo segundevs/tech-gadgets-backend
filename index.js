15d3fcd83d251d8384bfb04de59d95673bae2274
segundevs <tushbash@gmail.com>
2021-08-16 18:17:20 -0700

    implemented file uploads

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const productRoute = require('./routes/products');
@@ -15,6 +16,21 @@ mongoose.connect(process.env.MONGO_URL, {
   useCreateIndex: true
 }).then(console.log('connected to mongo db'))
   .catch((err)=>console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null, "images")
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name)
+  }
+})
+
+const upload = multer(storage);
+
+app.post('/api/upload', upload.single("file"), (req, res) => {
+  res.status(200).json('File successfully uploaded')
+})
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
