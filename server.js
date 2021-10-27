ded5a7a4f3d21aca67a07d0ee186ebf131366f55
segundevs <tushbash@gmail.com>
2021-10-26 22:55:29 -0700

    changes for deployment

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ app.use('/api/products', productRoute);
 
 
 //Port listerner
-app.listen('5000', ()=>{
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>{
   console.log('Backend is running....')
 })
 
